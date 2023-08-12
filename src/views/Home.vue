2c02d35a33f0fa4f010938e853d195a04af73a25
taqimfrm 08 <mustaqimabdullah08@gmail.com>
2023-08-12 19:27:25 +0800

    fixed list

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,7 +16,7 @@ export default {
     methods :{
       async GetAnime(){
       try{
-      const res = await axios.get('https://api.jikan.moe/v4/seasons/2023/winter');
+      const res = await axios.get('https://api.jikan.moe/v4/seasons/2023/summer');
       this.Animedata = res.data.data.slice(0,);
       console.log(this.Animedata);
       } catch (error) {
@@ -60,27 +60,30 @@ export default {
   <div class="lg:w-full block min-h-screen bg-slate-700">
     <div class="flex flex-wrap gap-2 justify-between py-[2rem]  mx-4 my-4 bg-white overflow-hidden">
       <div class="inline-flex max-w-[30%] h-[31.1rem] text-[14px] px-[0px] whitespace-normal  overflow-hidden" v-for="anime in Animedata"  :key="anime.mal_id">
-        <div class="block bg-slate-500 m-10">
+        <div class=" flex flex-col bg-slate-500 m-10">
           <div class="flex shadow-lg items-center h-[3rem] text-[1.2em] px-[0rem] font-bold leading-[1.1rem] weight-700 text-center whitespace-pre-line relative">
             <a class="my-0 mx-auto md:overflow-hidden text-ellipsis">
               {{ anime.title }} 
             </a>
           </div>
-          <div class="h-[120px] w-[240px] float-left relative ">
-            <img class="pl-2 py-2" :src ="anime.images.jpg.large_image_url" alt="Anime Image">
-          </div>
-          <div class=" bg-red-300 max-h-[325px] flex flex-col my-2">
-            <div class="shadow-lg text-center font-semibold">
-              {{ anime.source }}
+          <div>
+            <div class="h-[120px] w-[240px] float-left relative px-2">
+              <img class="pl-2 py-2" :src ="anime.images.jpg.large_image_url" alt="Anime Image">
             </div>
-            <div class="overflow-y-hidden hover:overflow-y-auto scrollbar scrollbar-track-cyan-200 text-left whitespace-normal p-[0.5em] leading-[1.2]">
-              {{ anime.synopsis }}
+            <div class=" max-h-[305px] flex flex-col my-2 mx-2">
+              <div class="shadow-sm shadow-gray-800 text-center font-semibold ">
+                {{ anime.source }}
+              </div>
+              <div class="overflow-y-hidden hover:overflow-y-auto scrollbar scrollbar-track-cyan-200 text-left whitespace-normal p-[0.5em] leading-[1.2]">
+                {{ anime.synopsis }}
+              </div> 
             </div> 
-          </div>
-          <div class="my-0 p-2 text-center bg-blue-500">
-              {{ anime.status }}
-          </div>
         </div>
+        <div class="mt-auto py-2 text-center bg-blue-700 font-bold">
+                {{ anime.status }}
+        </div>
+        </div>
+        
       </div>
     </div>
   </div>
